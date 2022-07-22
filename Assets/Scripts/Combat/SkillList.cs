e0bde706cd53299cc566fbd2226f684e45470675
qtarik2 <qtarik2@gmail.com>
2022-07-22 22:59:49 +0600

    Fix leveling loop and RSkill loop

--- Assets/Scripts/Combat/SkillList.cs
+++ Assets/Scripts/Combat/SkillList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-[CreateAssetMenu(menuName = "ScriptableObjects/SkillList")]
+[CreateAssetMenu(fileName = "new Spell", menuName = "ScriptableObjects/SkillList")]
 public class SkillList : ScriptableObject
 {
 
--- Assets/Scripts/General/ChampionDataManager.cs
+++ Assets/Scripts/General/ChampionDataManager.cs
@@ -26,8 +26,8 @@ public class ChampionDataManager : MonoBehaviour
         skillList.Add("eSkills", skills.eSkills);
         skillList.Add("rSkills", skills.rSkills);
 
-        ChampStats champStats = new ChampStats();
-        champStats.dynamicStatus["Bonus Physical Damage"] = true;
+        //ChampStats champStats = new ChampStats();
+        //champStats.dynamicStatus["Bonus Physical Damage"] = true;
     }
     // Start is called before the first frame update
     void Start()
@@ -71,16 +71,21 @@ public class ChampionDataManager : MonoBehaviour
     void SetChampionData()
     {
         var abilities = championDataResponse.abilities;
-
+        var val = 5;
         foreach (var title in skillTitle)
         {
+            if(title == "rSkills")
+            {
+                val = 3;
+            }
+            //if (counter > 4) return;
             if (!(skillList[title].Count > 0)) return;
 
             var effectInAbilities = abilities[skillKey[counter]][0].effects;
 
             for (int effect = 0; effect < effectInAbilities.Count; effect++)
             {
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < val; i++)
                 {
                     // Basic
                     if (abilities[skillKey[counter]][0].castTime != "none")
@@ -92,60 +97,64 @@ public class ChampionDataManager : MonoBehaviour
                         skillList[title][^1].basic.cost[i] = 0;
 
                     skillList[title][^1].basic.name = abilities[skillKey[counter]][0].name;
-                    skillList[title][^1].basic.coolDown[i] = (float)abilities[skillKey[counter]][0].cooldown.modifiers[0].values[i];
+                    if (abilities[skillKey[counter]][0].cooldown != null)
+                        skillList[title][^1].basic.coolDown[i] = (float)abilities[skillKey[counter]][0].cooldown.modifiers[0].values[i];
 
                     // Damage
                     // Effect need loop
                     // Modifiers need loop
                     if (effectInAbilities[effect].leveling.Count > 0)
                     {
-                        for (int modifier = 0; modifier < effectInAbilities[effect].leveling[0].modifiers.Count; modifier++)
+                        for (int level = 0; level < effectInAbilities[effect].leveling.Count; level++)
                         {
-                            if (effectInAbilities[effect].leveling[0].attribute == "Physical Damage" || effectInAbilities[effect].leveling[0].attribute == "Bonus Physical Damage")
+                            for (int modifier = 0; modifier < effectInAbilities[effect].leveling[level].modifiers.Count; modifier++)
                             {
-                                // Set Damage Type
-                                //skillList[title][^1].skillDamageType = "Physical";
+                                if (effectInAbilities[effect].leveling[level].attribute == "Physical Damage" || effectInAbilities[effect].leveling[level].attribute == "Bonus Physical Damage")
+                                {
+                                    // Set Damage Type
+                                    //skillList[title][^1].skillDamageType = "Physical";
 
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "")
-                                {
-                                    skillList[title][^1].damage.flatAD[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "")
+                                    {
+                                        skillList[title][^1].damage.flatAD[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "% bonus AD")
+                                    {
+                                        skillList[title][^1].damage.bonusAD[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "% AD")
+                                    {
+                                        skillList[title][^1].damage.percentAD[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
                                 }
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "% bonus AD")
+                                if (effectInAbilities[effect].leveling[level].attribute == "Magic Damage" || effectInAbilities[effect].leveling[level].attribute == "Total Magic Damage")
                                 {
-                                    skillList[title][^1].damage.bonusAD[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
-                                }
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "% AD")
-                                {
-                                    skillList[title][^1].damage.percentAD[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
-                                }
-                            }
-                            if (effectInAbilities[effect].leveling[0].attribute == "Magic Damage" || effectInAbilities[effect].leveling[0].attribute == "Total Magic Damage")
-                            {
-                                // Set Damage Type
-                                //skillList[title][^1].skillDamageType = "Physical";
+                                    // Set Damage Type
+                                    //skillList[title][^1].skillDamageType = "Physical";
 
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "")
-                                {
-                                    skillList[title][^1].damage.flatAP[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "")
+                                    {
+                                        skillList[title][^1].damage.flatAP[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "% bonus AP")
+                                    {
+                                        skillList[title][^1].damage.bonusAP[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "% AP")
+                                    {
+                                        skillList[title][^1].damage.percentAP[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
                                 }
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "% bonus AP")
+                                if (effectInAbilities[effect].leveling[level].attribute == "Bonus Attack Speed")
                                 {
-                                    skillList[title][^1].damage.bonusAP[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
-                                }
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "% AP")
-                                {
-                                    skillList[title][^1].damage.percentAP[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
-                                }
-                            }
-                            if (effectInAbilities[effect].leveling[0].attribute == "Bonus Attack Speed")
-                            {
-                                skillList[title][^1].selfEffects.ASIncrease = true;
-                                // Set Damage Type
-                                //skillList[title][^1].skillDamageType = "Physical";
+                                    skillList[title][^1].selfEffects.ASIncrease = true;
+                                    // Set Damage Type
+                                    //skillList[title][^1].skillDamageType = "Physical";
 
-                                if (effectInAbilities[effect].leveling[0].modifiers[modifier].units[0] == "%")
-                                {
-                                    skillList[title][^1].selfEffects.ASIncreasePercent[i] = (float)effectInAbilities[effect].leveling[0].modifiers[modifier].values[i];
+                                    if (effectInAbilities[effect].leveling[level].modifiers[modifier].units[0] == "%")
+                                    {
+                                        skillList[title][^1].selfEffects.ASIncreasePercent[i] = (float)effectInAbilities[effect].leveling[level].modifiers[modifier].values[i];
+                                    }
                                 }
                             }
                         }
