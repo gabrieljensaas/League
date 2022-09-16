35b46bef6e26ff384e87b191f9f51834ec85251c
Recep Şen <78692387+recep-sen@users.noreply.github.com>
2022-09-16 15:05:27 +0300

    Sett added

--- Assets/Scripts/Combat/Buffs/ShieldBuff.cs
+++ Assets/Scripts/Combat/Buffs/ShieldBuff.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class ShieldBuff : Buff
 {
     public string uniqueKey;
-    public ShieldBuff(float duration, BuffManager manager, string source, float shield, string uniqueKey) : base(manager)
+    public bool decaying = false;
+    public ShieldBuff(float duration, BuffManager manager, string source, float shield, string uniqueKey, bool decaying = false) : base(manager)
     {
+        this.decaying = decaying;
         value = shield;
         base.duration = float.MaxValue;
         base.source = source;
         manager.simulationManager.ShowText($"{manager.stats.name} Gained {shield} Shield from {source} for {duration} Seconds!");
         this.uniqueKey = uniqueKey;
+        this.decaying = decaying;
     }
 
     public override void Update()
     {
         duration -= Time.deltaTime;
+        if (decaying) value -= (Time.deltaTime / duration) * value;
         if (duration <= 0) Kill();
     }
     public override void Kill()
---
+++ Assets/Scripts/Combat/Checks/CheckForGrit.cs
@@ -0,0 +1,30 @@
+using Simulator.Combat;
+
+public class CheckForGrit : Check
+{
+    private Sett sett;
+    public CheckForGrit(ChampionCombat ccombat, Sett sett) : base(ccombat)
+    {
+        this.sett = sett;
+    }
+
+    public override float Control(float damage)
+    {
+        if (damage < (sett.myStats.maxHealth * 0.5f) - sett.grit)
+        {
+            sett.grit += damage;
+            sett.gritList.Add(new GritBuff(4, combat.myStats.buffManager, "Haymaker", damage, sett));
+        }
+        else
+        {
+            sett.gritList.Add(new GritBuff(4, combat.myStats.buffManager, "Haymaker", (sett.myStats.maxHealth * 0.5f) - sett.grit, sett));
+            sett.grit = sett.myStats.maxHealth * 0.5f;
+        }
+        return damage;
+    }
+
+    public override bool Control()
+    {
+        throw new System.NotImplementedException();
+    }
+}
