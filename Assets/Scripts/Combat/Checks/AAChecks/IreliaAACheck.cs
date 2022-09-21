c44402f4d8d28a9e9d1338a3b79b2cdadd765e59
Paolo Roxas <paoloroxas1@gmail.com>
2022-09-21 17:43:14 +0800

    implemented irelia

---
+++ Assets/Scripts/Combat/Checks/AAChecks/IreliaAACheck.cs
@@ -0,0 +1,30 @@
+using Simulator.Combat;
+
+public class IreliaAACheck : Check
+{
+    public IreliaAACheck(ChampionCombat ccombat) : base(ccombat)
+    {
+    }
+
+    public override float Control(float damage)
+    {
+        if (combat.myStats.buffManager.buffs.TryGetValue("IonianFervor", out Buff buff))
+        {
+            if(combat.myStats.buffManager.buffs.TryGetValue("IonianFervorAS", out Buff ASbuff))
+                ASbuff.Kill();
+                
+            combat.myStats.buffManager.buffs.Add("IonianFervorAS", new AttackSpeedBuff(6, combat.myStats.buffManager, "IonianFervor", Irelia.IonianFervorAttackSpeed(combat.myStats.level, (int)buff.value), "IonianFervorAS"));
+
+            if (buff.value == 4)
+            {
+                damage += Irelia.IonianFervorEmpoweredDamage(combat.myStats.level) + combat.myStats.bonusAD;
+            }
+        }
+        return damage;
+    }
+
+    public override bool Control()
+    {
+        throw new System.NotImplementedException();
+    }
+}
