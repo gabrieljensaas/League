1c56929de7e173ab17c9a39e74857e7cb61af8a7
Nischal Joshi <076bct051@ioepc.edu.np>
2022-10-07 19:38:04 +0545

    TwistedFate Added

---
+++ Assets/Scripts/Combat/Champions/TwistedFate.cs
@@ -0,0 +1,114 @@
+using Simulator.Combat;
+using System.Collections;
+
+public class TwistedFate : ChampionCombat
+{
+
+	private bool wCast;
+    private bool wBlueCard;
+    private bool wGoldCard;
+    private int eStack;
+    public override void UpdatePriorityAndChecks()
+    {
+        combatPrio = new string[] { "Q", "W", "E", "R", "A" };
+
+        checksQ.Add(new CheckCD(this, "Q"));
+        checksW.Add(new CheckCD(this, "W"));
+        checksE.Add(new CheckCD(this, "E"));
+        checksR.Add(new CheckCD(this, "R"));
+        checksA.Add(new CheckCD(this, "A"));
+
+        checksQ.Add(new CheckIfCasting(this));
+        checksW.Add(new CheckIfCasting(this));
+        checksE.Add(new CheckIfCasting(this));
+        checksR.Add(new CheckIfCasting(this));
+        checksA.Add(new CheckIfCasting(this));
+
+        checksQ.Add(new CheckIfDisrupt(this));
+        checksW.Add(new CheckIfDisrupt(this));
+        checksE.Add(new CheckIfDisrupt(this));
+        checksR.Add(new CheckIfDisrupt(this));
+        checksA.Add(new CheckIfTotalCC(this));
+        checksA.Add(new CheckIfDisarmed(this));
+
+        qKeys.Add("Magic Damage");
+        wKeys.Add("Magic Damage"); //Blue Card
+        wKeys.Add("Magic Damage"); //Red Card
+        wKeys.Add("Magic Damage"); //Gold Card
+        wKeys.Add("Stun Duration");
+        eKeys.Add("Bonus Attack Speed");
+        eKeys.Add("Bonus Magic Damage");
+
+        MyBuffManager.Add("EPassiveBuff", new AttackSpeedBuff(float.MaxValue, MyBuffManager, ESkill().basic.name, ESkill().UseSkill(myStats.eLevel, eKeys[0], myStats, targetStats), "AttackSpeed"));
+        base.UpdatePriorityAndChecks();
+    }
+
+    public override IEnumerator ExecuteQ()
+    {
+        if (!CheckForAbilityControl(checksQ)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(QSkill().basic.castTime));
+        UpdateAbilityTotalDamage(ref qSum, 0, QSkill(), myStats.qLevel, qKeys[0]);
+        myStats.qCD = QSkill().basic.coolDown[4];
+    }
+
+    public override IEnumerator ExecuteW()
+    {
+        if (!CheckForAbilityControl(checksW)) yield break;
+
+        if (!wCast)
+        {
+            yield return StartCoroutine(StartCastingAbility(myStats.wSkill[0].basic.castTime));
+        }
+        else
+        {
+            yield return StartCoroutine(StartCastingAbility(myStats.wSkill[0].basic.castTime));
+            if (myStats.wSkill[0].UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats) >= targetStats.currentHealth)
+            {
+                wBlueCard = true;
+                AutoAttack(new Damage(WSkill().UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats), SkillDamageType.Spell));
+            }
+            else
+            {
+                wGoldCard = true;
+                TargetBuffManager.Add("StunBuff", new StunBuff(WSkill().UseSkill(myStats.wLevel, wKeys[3], myStats, targetStats), TargetBuffManager, "StunBuff"));
+                AutoAttack(new Damage(WSkill().UseSkill(myStats.wLevel, wKeys[2], myStats, targetStats), SkillDamageType.Spell));
+            }
+            myStats.wCD = myStats.wSkill[0].basic.coolDown[4];
+        }
+    }
+    public override IEnumerator ExecuteA()
+    {
+        if (!CheckForAbilityControl(checksA)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(0.1f));
+        if(eStack >= 3)
+		{
+            if (wBlueCard)
+            {
+                AutoAttack(new Damage(myStats.AD + WSkill().UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats) + ESkill().UseSkill(myStats.eLevel, eKeys[1], myStats, targetStats), SkillDamageType.PhysAndSpell));
+            }
+            else if (wGoldCard)
+            {
+                AutoAttack(new Damage(myStats.AD + WSkill().UseSkill(myStats.wLevel, wKeys[2], myStats, targetStats) + ESkill().UseSkill(myStats.eLevel, eKeys[1], myStats, targetStats), SkillDamageType.PhysAndSpell));
+            }
+            else
+                AutoAttack(new Damage(myStats.AD + ESkill().UseSkill(myStats.eLevel, eKeys[1], myStats, targetStats), SkillDamageType.Phyiscal));
+            eStack = 0;
+        }
+		else
+		{
+            if (wBlueCard)
+            {
+                AutoAttack(new Damage(myStats.AD + WSkill().UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats), SkillDamageType.PhysAndSpell));
+            }
+            else if (wGoldCard)
+            {
+                AutoAttack(new Damage(myStats.AD + WSkill().UseSkill(myStats.wLevel, wKeys[2], myStats, targetStats), SkillDamageType.PhysAndSpell));
+            }
+            else
+                AutoAttack(new Damage(myStats.AD, SkillDamageType.Phyiscal));
+            eStack++;
+        }
+    }
+}
