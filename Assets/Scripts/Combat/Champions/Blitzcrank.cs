28f983c7232af004fd571ac5aee453d95b5668a9
Nischal Joshi <076bct051@ioepc.edu.np>
2022-10-19 22:02:58 +0545

    Blitzcrank - Incomplete

---
+++ Assets/Scripts/Combat/Champions/Blitzcrank.cs
@@ -0,0 +1,88 @@
+using Simulator.Combat;
+using System.Collections;
+
+public class Blitzcrank : ChampionCombat
+{
+    public override void UpdatePriorityAndChecks()
+    {
+        combatPrio = new string[] { "E", "W", "Q", "R", "A" };
+
+        checksQ.Add(new CheckCD(this, "Q"));
+        checksW.Add(new CheckCD(this, "W"));
+        checksE.Add(new CheckCD(this, "E"));
+        checksR.Add(new CheckCD(this, "R"));
+        checksA.Add(new CheckCD(this, "A"));
+
+        checksQ.Add(new CheckIfCasting(this));
+        checksW.Add(new CheckIfCasting(this));
+        checksE.Add(new CheckIfCasting(this));
+        checksR.Add(new CheckIfCasting(this));
+        checksA.Add(new CheckIfCasting(this));
+
+        checksQ.Add(new CheckIfDisrupt(this));
+        checksW.Add(new CheckIfDisrupt(this));
+        checksE.Add(new CheckIfDisrupt(this));
+        checksR.Add(new CheckIfDisrupt(this));
+        checksA.Add(new CheckIfTotalCC(this));
+        checksA.Add(new CheckIfDisarmed(this));
+
+        targetCombat.checksQ.Add(new CheckIfEnemyTargetable(targetCombat));
+        targetCombat.checksW.Add(new CheckIfEnemyTargetable(targetCombat));
+        targetCombat.checksE.Add(new CheckIfEnemyTargetable(targetCombat));
+        targetCombat.checksR.Add(new CheckIfEnemyTargetable(targetCombat));
+        targetCombat.checksA.Add(new CheckIfEnemyTargetable(targetCombat));
+
+        qKeys.Add("Magic Damage");
+
+        wKeys.Add("Bonus Attack Speed");
+
+        rKeys.Add("Magic Damage");
+        rKeys.Add("Magic Damage");
+
+        base.UpdatePriorityAndChecks();
+    }
+
+    public override IEnumerator ExecuteQ()
+    {
+        if (!CheckForAbilityControl(checksQ) || myStats.qLevel == 0) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(QSkill().basic.castTime));
+        UpdateAbilityTotalDamage(ref qSum, 0, QSkill(), myStats.qLevel, qKeys[0]); UpdateAbilityTotalDamage(ref qSum, 0, new Damage(QSkill().UseSkill(myStats.qLevel, qKeys[0], myStats, targetStats), SkillDamageType.True), QSkill().basic.name);
+        myStats.qCD = QSkill().basic.coolDown[myStats.qLevel];
+    }
+
+    public override IEnumerator ExecuteW()
+    {
+        if (!CheckForAbilityControl(checksW) || myStats.wLevel == 0) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(WSkill().basic.castTime));
+        UpdateAbilityTotalDamage(ref wSum, 1, WSkill(), myStats.wLevel, wKeys[0]);
+        myStats.wCD = WSkill().basic.coolDown[myStats.wLevel];
+    }
+
+    public override IEnumerator ExecuteE()
+    {
+        if (!CheckForAbilityControl(checksE) || myStats.eLevel == 0) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(ESkill().basic.castTime));
+        UpdateAbilityTotalDamage(ref eSum, 2, ESkill(), myStats.eLevel, eKeys[0]);
+        myStats.eCD = ESkill().basic.coolDown[myStats.eLevel];
+    }
+
+    public override IEnumerator ExecuteR()
+    {
+        if (!CheckForAbilityControl(checksR) || myStats.rLevel == 0) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(RSkill().basic.castTime));
+        UpdateAbilityTotalDamage(ref rSum, 3, RSkill(), myStats.rLevel, rKeys[0]);
+        myStats.rCD = RSkill().basic.coolDown[myStats.rLevel];
+    }
+
+    public override IEnumerator ExecuteA()
+    {
+        if (!CheckForAbilityControl(checksA)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(0.1f));
+        AutoAttack(new Damage(myStats.AD, SkillDamageType.Phyiscal));
+    }
+}
