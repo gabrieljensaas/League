a6c54e9e316730c1c806d34e94617fda6a316176
recep-sen <devrecepsen@gmail.com>
2022-11-22 18:27:36 +0300

    Kaisa updated

--- Assets/Scripts/Combat/Champions/Kaisa.cs
+++ Assets/Scripts/Combat/Champions/Kaisa.cs
@@ -29,7 +29,6 @@ public class Kaisa : ChampionCombat
     {
         combatPrio = new string[] { "R", "E", "W", "Q", "A" };
 
-        autoattackcheck = new KaisaAACheck(this, this);
         checksQ.Add(new CheckCD(this, "Q"));
         checksW.Add(new CheckCD(this, "W"));
         checksE.Add(new CheckCD(this, "E"));
@@ -54,9 +53,9 @@ public class Kaisa : ChampionCombat
         checksR.Add(new CheckIfImmobilize(this));
         checksA.Add(new CheckIfDisarmed(this));
         checkTakeDamagePostMitigation.Add(new CheckShield(this));
-        checkTakeDamagePostMitigation.Add(new CheckShield(this));
 
-        qKeys.Add("Total Evolved Single-Target Damage");
+        qKeys.Add("Physical Damage Per Missile");
+        qKeys.Add("Reduced Damage Per Missile");
         wKeys.Add("Magic Damage");
         eKeys.Add("Bonus Attack Speed");
         rKeys.Add("Shield Strength");
@@ -66,20 +65,39 @@ public class Kaisa : ChampionCombat
 
     public override IEnumerator ExecuteQ()
     {
+        if (myStats.qLevel == -1) yield break;
         if (!CheckForAbilityControl(checksQ)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(myStats.qSkill[0].basic.castTime));
-        myStats.qCD = myStats.qSkill[0].basic.coolDown[4];
+        myStats.qCD = myStats.qSkill[0].basic.coolDown[myStats.qLevel];
+        UpdateTotalDamage(ref qSum, 0, new Damage(0, SkillDamageType.Phyiscal, (SkillComponentTypes)2048), QSkill().basic.name);
         yield return new WaitForSeconds(0.4f);                                //missle travel time
-        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], 4, qKeys[0]);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[0],skillComponentTypes: (SkillComponentTypes)16516);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1],skillComponentTypes: (SkillComponentTypes)8324);
+        yield return new WaitForSeconds(0.1f);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        yield return new WaitForSeconds(0.1f);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        yield return new WaitForSeconds(0.1f);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        yield return new WaitForSeconds(0.1f);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        yield return new WaitForSeconds(0.1f);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
+        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[1], skillComponentTypes: (SkillComponentTypes)8324);
     }
     public override IEnumerator ExecuteW()
     {
+        if (myStats.wLevel == -1) yield break;
         if (!CheckForAbilityControl(checksW)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(myStats.wSkill[0].basic.castTime));
-        UpdateTotalDamage(ref wSum, 1, myStats.wSkill[0], 4, wKeys[0]);
-        myStats.wCD = myStats.wSkill[0].basic.coolDown[4];
+        UpdateTotalDamage(ref wSum, 1, myStats.wSkill[0], myStats.wLevel, wKeys[0], skillComponentTypes: (SkillComponentTypes)34948);
+        myStats.wCD = myStats.wSkill[0].basic.coolDown[myStats.wLevel];
         if (targetStats.buffManager.buffs.TryGetValue("Plasma", out Buff value))
         {
             value.value += 3;
@@ -94,30 +112,70 @@ public class Kaisa : ChampionCombat
         {
             PlasmaBuff buff = new PlasmaBuff(4, targetStats.buffManager, "Kaisa's Passive");
             buff.value = 3;
-            targetStats.buffManager.buffs.Add("Plasma", buff);
+            MyBuffManager.buffs.Add("Plasma", buff);
         }
         myStats.wCD *= 0.33f;
     }
 
     public override IEnumerator ExecuteE()
     {
+        if (myStats.eLevel == -1) yield break;
         if (!CheckForAbilityControl(checksE)) yield break;
 
         myStats.eSkill[0].basic.castTime = GetKaisaECastTime(myStats.bonusAS);
 
         yield return StartCoroutine(StartCastingAbility(myStats.eSkill[0].basic.castTime));
-        myStats.buffManager.buffs.Add("Untargetable", new UntargetableBuff(0.5f, myStats.buffManager, myStats.eSkill[0].basic.name));
-        myStats.buffManager.buffs.Add(myStats.eSkill[0].basic.name, new AttackSpeedBuff(4, myStats.buffManager, myStats.eSkill[0].basic.name, myStats.wSkill[0].UseSkill(4, eKeys[0], myStats, targetStats), myStats.eSkill[0].basic.name));
-        myStats.eCD = myStats.eSkill[0].basic.coolDown[4];
+        MyBuffManager.buffs.Add("Untargetable", new UntargetableBuff(0.5f, myStats.buffManager, myStats.eSkill[0].basic.name));
+        MyBuffManager.Add(myStats.eSkill[0].basic.name, new AttackSpeedBuff(4, myStats.buffManager, myStats.eSkill[0].basic.name,
+            myStats.wSkill[0].UseSkill(myStats.eLevel, eKeys[0], myStats, targetStats), myStats.eSkill[0].basic.name));
+        UpdateTotalDamage(ref eSum, 2, new Damage(0, SkillDamageType.Phyiscal, buffNames:new string[] {"Untargetable", ESkill().basic.name },
+            skillComponentType: (SkillComponentTypes)2048), ESkill().basic.name);
+        myStats.eCD = myStats.eSkill[0].basic.coolDown[myStats.eLevel];
     }
 
     public override IEnumerator ExecuteR()
     {
+        if (myStats.rLevel == -1) yield break;
         if (!CheckForAbilityControl(checksR)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(myStats.rSkill[0].basic.castTime));
-        myStats.buffManager.shields.Add(myStats.rSkill[0].basic.name, new ShieldBuff(2, myStats.buffManager, myStats.rSkill[0].basic.name, myStats.rSkill[0].UseSkill(2, rKeys[0], myStats, targetStats), myStats.rSkill[0].basic.name));
-        myStats.rCD = myStats.rSkill[0].basic.coolDown[2];
+        myStats.buffManager.shields.Add(myStats.rSkill[0].basic.name, new ShieldBuff(2, myStats.buffManager, myStats.rSkill[0].basic.name,
+            myStats.rSkill[0].UseSkill(myStats.rLevel, rKeys[0], myStats, targetStats), myStats.rSkill[0].basic.name));
+        UpdateTotalDamage(ref rSum, 3, 
+            new Damage(0, SkillDamageType.Phyiscal, buffNames: new string[] {RSkill().basic.name}, skillComponentType: (SkillComponentTypes)2050), RSkill().basic.name);
+        myStats.rCD = myStats.rSkill[0].basic.coolDown[myStats.rLevel];
+    }
+
+    public override IEnumerator ExecuteA()
+    {
+        if (!CheckForAbilityControl(checksA)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(0.1f));
+        if(UpdateTotalDamage(ref aSum, 5, new Damage(myStats.AD, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)5916), "Kai'Sa's Auto Attack") != float.MinValue)
+        {
+            if (TargetBuffManager.buffs.TryGetValue("Plasma", out Buff value))
+            {
+                UpdateTotalDamage(ref pSum, 4, 
+                    new Damage(GetKaisaPassiveDamageByLevel(myStats.level, (int)value.value, myStats.AP), SkillDamageType.Spell, skillComponentType: (SkillComponentTypes)36), "Second Skin");
+                if (value.value == 5)
+                {
+                    DealPassiveDamage((targetStats.maxHealth - targetStats.currentHealth) / 100 * (15 + (5 * (myStats.AP % 100))));
+                    value.Kill();
+                }
+                else
+                {
+                    value.value++;
+                    value.duration = 4f;
+                }
+            }
+            else
+            {
+                TargetBuffManager.Add("Plasma", new PlasmaBuff(4, targetStats.buffManager, "Kai'Sa's Auto Attacks"));
+            }
+            myStats.eCD -= 0.5f;
+        }
+        attackCooldown = 1f / myStats.attackSpeed;
+        simulationManager.AddCastLog(myCastLog, 5);
     }
 
     public override IEnumerator HijackedR(int skillLevel)
@@ -130,6 +188,6 @@ public class Kaisa : ChampionCombat
 
     public void DealPassiveDamage(float rawdamage)
     {
-        UpdateTotalDamage(ref pSum, 4, new Damage(rawdamage, SkillDamageType.Spell), myStats.passiveSkill.skillName);
+        UpdateTotalDamage(ref pSum, 4, new Damage(rawdamage, SkillDamageType.Spell, skillComponentType: (SkillComponentTypes)36), myStats.passiveSkill.skillName);
     }
 }
