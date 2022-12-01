5b7c9bc1e5f13c5f4d340c096020f3d3d534e665
recep-sen <devrecepsen@gmail.com>
2022-12-01 17:12:39 +0300

    Syndra added execute fixed

---
+++ Assets/Scripts/Combat/Champions/Syndra.cs
@@ -0,0 +1,154 @@
+using Simulator.Combat;
+using System.Collections;
+using UnityEngine;
+
+public class Syndra : ChampionCombat
+{
+    public int TranscendentStack;
+    public float SpheresOnTheGround = 0;
+    public int QStack = 1;
+    public int MaxQStack = 1;
+    public float QRecharge = 0;
+    
+    public override void UpdatePriorityAndChecks()
+    {
+        combatPrio = new string[] { "R", "Q", "W", "E", "A" };
+
+        checksQ.Add(new CheckCD(this, "Q"));
+        checksW.Add(new CheckCD(this, "W"));
+        checksE.Add(new CheckCD(this, "E"));
+        checksR.Add(new CheckCD(this, "R"));
+        checksA.Add(new CheckCD(this, "A"));
+        checksQ.Add(new CheckIfCasting(this));
+        checksW.Add(new CheckIfCasting(this));
+        checksE.Add(new CheckIfCasting(this));
+        checksR.Add(new CheckIfCasting(this));
+        checksA.Add(new CheckIfCasting(this));
+        checksQ.Add(new CheckIfDisrupt(this));
+        checksW.Add(new CheckIfDisrupt(this));
+        checksE.Add(new CheckIfDisrupt(this));
+        checksR.Add(new CheckIfDisrupt(this));
+        checksA.Add(new CheckIfTotalCC(this));
+        checksR.Add(new CheckIfImmobilize(this));
+        checksA.Add(new CheckIfDisarmed(this));
+        checksR.Add(new CheckIfExecutes(this, "Syndra",ref TranscendentStack, ref SpheresOnTheGround));
+
+        qKeys.Add("Magic Damage");
+        wKeys.Add("Magic Damage");
+        eKeys.Add("Magic Damage");
+        rKeys.Add("Magic Damage per Sphere");
+
+        if (TranscendentStack >= 120) //increase ability power by %15
+            if (TranscendentStack >= 40) MaxQStack = 2;
+        if(myStats.rLevel >= 0) //q gains ability haste
+
+        base.UpdatePriorityAndChecks();
+    }
+
+    public override void CombatUpdate()
+    {
+        base.CombatUpdate();
+
+        QRecharge += Time.fixedDeltaTime;
+
+        if(QRecharge >= QSkill().basic.coolDown[myStats.qLevel] && QStack < MaxQStack)
+        {
+            QStack++;
+            QRecharge= 0;
+        }
+    }
+
+    public override IEnumerator ExecuteQ()
+    {
+        if (myStats.qLevel == -1 || QStack <= 0) yield break;
+        if (!CheckForAbilityControl(checksQ)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(QSkill().basic.castTime));
+        UpdateTotalDamage(ref qSum, 0, new Damage(0, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)2048), QSkill().basic.name);
+        myStats.qCD = 1.25f;
+        yield return new WaitForSeconds(0.6f);
+        UpdateTotalDamage(ref qSum, 0, QSkill(), myStats.qLevel, qKeys[0], skillComponentTypes: (SkillComponentTypes)16512);
+        StartCoroutine(PutSphereOnTheGround());
+        QRecharge = 0;
+        simulationManager.AddCastLog(myCastLog, 0);
+    }
+
+    public override IEnumerator ExecuteW()
+    {
+        if (myStats.wLevel == -1) yield break;
+        if (!CheckForAbilityControl(checksW)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(myStats.wSkill[0].basic.castTime));
+        myStats.wCD = myStats.wSkill[0].basic.coolDown[myStats.wLevel];
+        if(UpdateTotalDamage(ref wSum, 1, myStats.wSkill[0], myStats.wLevel, wKeys[0], skillComponentTypes: (SkillComponentTypes)18560) != float.MinValue && TranscendentStack >= 60)
+        {
+            UpdateTotalDamage(ref wSum, 1,
+                new Damage(WSkill().UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats) * (0.15f + (0.015f * myStats.AP)), SkillDamageType.True, skillComponentType: (SkillComponentTypes)16384), WSkill().basic.name);
+        }
+        StopCoroutine(PutSphereOnTheGround());
+        SpheresOnTheGround--;
+        StartCoroutine(PutSphereOnTheGround());
+        simulationManager.AddCastLog(myCastLog, 1);
+    }
+
+    public override IEnumerator ExecuteE()
+    {
+        if (myStats.eLevel == -1) yield break;
+        if (!CheckForAbilityControl(checksE)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(myStats.eSkill[0].basic.castTime));
+        if(UpdateTotalDamage(ref eSum, 2, myStats.eSkill[0], myStats.eLevel, eKeys[0], skillComponentTypes: (SkillComponentTypes)18560) != float.MinValue)
+        {
+            TargetBuffManager.Add("Airborne", new AirborneBuff(0.1f, TargetBuffManager, ESkill().basic.name));
+            if(SpheresOnTheGround > 0) TargetBuffManager.Add("Stun", new StunBuff(1.25f, TargetBuffManager, ESkill().basic.name));
+        }
+        myStats.eCD = myStats.eSkill[0].basic.coolDown[myStats.eLevel];
+        simulationManager.AddCastLog(myCastLog, 2);
+    }
+
+    public override IEnumerator ExecuteR()
+    {
+        if (myStats.rLevel == -1) yield break;
+        if (!CheckForAbilityControl(checksR)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(myStats.rSkill[0].basic.castTime + 0.264f));
+        UpdateTotalDamage(ref rSum, 3, myStats.rSkill[0], myStats.rLevel, rKeys[0], skillComponentTypes: (SkillComponentTypes)34948);
+        UpdateTotalDamage(ref rSum, 3, myStats.rSkill[0], myStats.rLevel, rKeys[0], skillComponentTypes: (SkillComponentTypes)32900);
+        UpdateTotalDamage(ref rSum, 3, myStats.rSkill[0], myStats.rLevel, rKeys[0], skillComponentTypes: (SkillComponentTypes)32900);
+
+        int maxSphere = 0;
+        while(SpheresOnTheGround > 0)
+        {
+            StopCoroutine(PutSphereOnTheGround());
+            UpdateTotalDamage(ref rSum, 3, myStats.rSkill[0], myStats.rLevel, rKeys[0], skillComponentTypes: (SkillComponentTypes)32900);
+            SpheresOnTheGround -= 1;
+            maxSphere++;
+            if (maxSphere == 4) break;
+        }
+        for(int i = 0; i < 3 + maxSphere; i++)
+        {
+            StartCoroutine(PutSphereOnTheGround());
+        }
+        myStats.rCD = myStats.rSkill[0].basic.coolDown[myStats.rLevel];
+        if (TranscendentStack >= 100 && targetStats.PercentCurrentHealth <= 0.15f)
+            UpdateTotalDamage(ref rSum, 3, new Damage(targetStats.currentHealth, SkillDamageType.True), RSkill().basic.name); //execute
+        simulationManager.AddCastLog(myCastLog, 3);
+    }
+
+    public override IEnumerator ExecuteA()
+    {
+        if (!CheckForAbilityControl(checksA)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(0.1f));
+        UpdateTotalDamage(ref aSum, 5, new Damage(myStats.AD, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)5916), "Syndra's Auto Attack");
+        attackCooldown = 1f / myStats.attackSpeed;
+        simulationManager.AddCastLog(myCastLog, 5);
+    }
+
+    public IEnumerator PutSphereOnTheGround()
+    {
+        SpheresOnTheGround++;
+        yield return new WaitForSeconds(6f);
+        SpheresOnTheGround--;
+    }
+}
--- Assets/Scripts/Combat/Checks/CheckIfExecutes.cs
+++ Assets/Scripts/Combat/Checks/CheckIfExecutes.cs
@@ -5,16 +5,38 @@ public class CheckIfExecutes : Check
     private string skill;
     private int stack;
     private float multiplier;
-    public CheckIfExecutes(ChampionCombat ccombat, string skill, int stack = 0, float multiplier = 1) : base(ccombat)
+    public CheckIfExecutes(ChampionCombat ccombat, string skill,ref int stack,ref float multiplier) : base(ccombat)
     {
         this.skill = skill;
         this.stack = stack;
         this.multiplier = multiplier;
     }
 
+    public CheckIfExecutes(ChampionCombat ccombat, string skill, ref int stack) : base(ccombat)
+    {
+        this.skill = skill;
+        this.stack = stack;
+        this.multiplier = 1;
+    }
+
+    public CheckIfExecutes(ChampionCombat ccombat, string skill, ref float multiplier) : base(ccombat)
+    {
+        this.skill = skill;
+        this.stack = 0;
+        this.multiplier = multiplier;
+    }
+
+    public CheckIfExecutes(ChampionCombat ccombat, string skill) : base(ccombat)
+    {
+        this.skill = skill;
+        this.stack = 0;
+        this.multiplier = 1;
+    }
+
     public override Damage Control(Damage damage)
     {
         throw new System.NotImplementedException();
+
     }
 
     public override bool Control()
@@ -22,23 +44,44 @@ public class CheckIfExecutes : Check
         switch (skill)
         {
             case "Q":
-                return combat.QSkill().UseSkill(combat.myStats.qLevel, combat.qKeys[0], combat.myStats, combat.targetStats) >= combat.targetStats.currentHealth;
+                var damageq = combat.QSkill().UseSkill(combat.myStats.qLevel, combat.qKeys[0], combat.myStats, combat.targetStats);
+                if (combat.QSkill().skillDamageType == SkillDamageType.Phyiscal) damageq = (int)(damageq * 100 / (100 + combat.targetStats.armor));
+                else if (combat.WSkill().skillDamageType == SkillDamageType.Spell) damageq = (int)(damageq * 100 / (100 + combat.targetStats.spellBlock));
+                else if (combat.WSkill().skillDamageType == SkillDamageType.True) damageq = (int)damageq;
+                return damageq >= combat.targetStats.currentHealth;
             case "W":
-                return combat.WSkill().UseSkill(combat.myStats.wLevel, combat.wKeys[0], combat.myStats, combat.targetStats) >= combat.targetStats.currentHealth;
+                var damagew = combat.WSkill().UseSkill(combat.myStats.qLevel, combat.qKeys[0], combat.myStats, combat.targetStats);
+                if (combat.WSkill().skillDamageType == SkillDamageType.Phyiscal) damagew = (int)(damagew * 100 / (100 + combat.targetStats.armor));
+                else if (combat.WSkill().skillDamageType == SkillDamageType.Spell) damagew = (int)(damagew * 100 / (100 + combat.targetStats.spellBlock));
+                else if (combat.WSkill().skillDamageType == SkillDamageType.True) damagew = (int)damagew;
+                return damagew >= combat.targetStats.currentHealth;
             case "E":
-                return combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[0], combat.myStats, combat.targetStats) >= combat.targetStats.currentHealth;
+                var damagee = combat.WSkill().UseSkill(combat.myStats.qLevel, combat.qKeys[0], combat.myStats, combat.targetStats);
+                if (combat.WSkill().skillDamageType == SkillDamageType.Phyiscal) damagee = (int)(damagee * 100 / (100 + combat.targetStats.armor));
+                else if (combat.WSkill().skillDamageType == SkillDamageType.Spell) damagee = (int)(damagee * 100 / (100 + combat.targetStats.spellBlock));
+                else if (combat.WSkill().skillDamageType == SkillDamageType.True) damagee = (int)damagee;
+                return damagee >= combat.targetStats.currentHealth;
             case "R":
-                return (combat.RSkill().UseSkill(combat.myStats.rLevel, combat.rKeys[0], combat.myStats, combat.targetStats) * multiplier) + stack >= combat.targetStats.currentHealth;
+                var damager = (combat.RSkill().UseSkill(combat.myStats.rLevel, combat.rKeys[0], combat.myStats, combat.targetStats) * multiplier) + stack;
+                if (combat.RSkill().skillDamageType == SkillDamageType.Phyiscal) damager = (int)(damager * 100 / (100 + combat.targetStats.armor));
+                else if (combat.RSkill().skillDamageType == SkillDamageType.Spell) damager = (int)(damager * 100 / (100 + combat.targetStats.spellBlock));
+                else if (combat.RSkill().skillDamageType == SkillDamageType.True) damager = (int)damager;
+                return damager >= combat.targetStats.currentHealth;
             case "Riven":
-                return combat.RSkill(1).UseSkill(combat.myStats.rLevel, combat.rKeys[0], combat.myStats, combat.targetStats) * (1 + ((combat.targetStats.maxHealth - combat.targetStats.currentHealth) / combat.targetStats.maxHealth) > 0.75f ? 2 : (combat.targetStats.maxHealth - combat.targetStats.currentHealth) * 2.667f) >= combat.targetStats.currentHealth;
-            case "SylasRiven":
+                return (int)(combat.RSkill(1).UseSkill(combat.myStats.rLevel, combat.rKeys[0], combat.myStats, combat.targetStats) * (1 + ((combat.targetStats.maxHealth - combat.targetStats.currentHealth) / combat.targetStats.maxHealth) * 100 / (100 + combat.targetStats.armor)) > 0.75f ? 2 : (combat.targetStats.maxHealth - combat.targetStats.currentHealth) * 2.667f) >= combat.targetStats.currentHealth;
+            /*case "SylasRiven":
                 return combat.RSkill(1).UseSkill(combat.myStats.rLevel, combat.rKeys[0], combat.targetStats, combat.myStats) * (1 + ((combat.myStats.maxHealth - combat.myStats.currentHealth) / combat.myStats.maxHealth) > 0.75f ? 2 : (combat.myStats.maxHealth - combat.myStats.currentHealth) * 2.667f) >= combat.myStats.currentHealth;
+            */
             case "Kalista":
                 if (combat.TargetBuffManager.buffs.TryGetValue("Rend", out Buff value))
                 {
-                    return combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[0], combat.myStats, combat.targetStats) + ((value.value - 1) * combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[1], combat.myStats, combat.targetStats)) >= combat.targetStats.currentHealth;
+                    return (int)(combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[0], combat.myStats, combat.targetStats) + ((value.value - 1) * combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[1], combat.myStats, combat.targetStats)) * 100 / (100 + combat.targetStats.armor)) >= combat.targetStats.currentHealth;
                 }
-                return combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[0], combat.myStats, combat.targetStats) >= combat.targetStats.currentHealth;
+                return (int)(combat.ESkill().UseSkill(combat.myStats.eLevel, combat.eKeys[0], combat.myStats, combat.targetStats) * 100 / (100 + combat.targetStats.armor)) >= combat.targetStats.currentHealth;
+            case "Syndra":
+                var damageSyndra = (combat.RSkill().UseSkill(combat.myStats.rLevel, combat.rKeys[0], combat.myStats, combat.targetStats) * multiplier) + stack + 3;
+                damageSyndra = (int)(damageSyndra * 100 / (100 + combat.targetStats.spellBlock));
+                return stack >= 100 ? damageSyndra >= combat.targetStats.currentHealth - (combat.targetStats.maxHealth * 0.15f) : damageSyndra >= combat.targetStats.currentHealth;
             default:
                 return false;
         }
