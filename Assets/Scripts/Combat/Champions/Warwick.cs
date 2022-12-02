fa0e44267c4f44dc560fa30301f146f6eecfaec7
Nischal Joshi <076bct051@ioepc.edu.np>
2022-12-02 16:07:18 +0545

    Warwick Updated

--- Assets/Scripts/Combat/Champions/Warwick.cs
+++ Assets/Scripts/Combat/Champions/Warwick.cs
@@ -60,17 +60,30 @@ public class Warwick : ChampionCombat
 
     public override IEnumerator ExecuteQ()
     {
+        if (myStats.qLevel == -1) yield break;
         if (!CheckForAbilityControl(checksQ)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(QSkill().basic.castTime));
-        UpdateTotalDamage(ref qSum, 0, new Damage(1.2f * myStats.AD, SkillDamageType.Phyiscal), QSkill().basic.name);
-        UpdateTotalDamage(ref qSum, 0, new Damage(QSkill().UseSkill(myStats.qLevel, qKeys[0], myStats, targetStats) * targetStats.maxHealth, SkillDamageType.Spell), QSkill().basic.name);
+        UpdateTotalDamage(ref qSum, 0, new Damage(1.2f * myStats.AD, SkillDamageType.Phyiscal, skillComponentType:(SkillComponentTypes)792), QSkill().basic.name);
+        UpdateTotalDamage(ref qSum, 0, new Damage(QSkill().UseSkill(myStats.qLevel, qKeys[0], myStats, targetStats) * targetStats.maxHealth, SkillDamageType.Spell, skillComponentType:(SkillComponentTypes)34944), QSkill().basic.name);
         UpdateTotalHeal(ref qSum, QSkill().UseSkill(myStats.qLevel, qKeys[1], myStats, targetStats), QSkill().basic.name);
         myStats.qCD = QSkill().basic.coolDown[myStats.qLevel];
+        simulationManager.AddCastLog(myCastLog, 0);
+    }
+    public override IEnumerator ExecuteW()
+    {
+        if (myStats.wLevel == -1) yield break;
+        if (!CheckForAbilityControl(checksW)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(WSkill().basic.castTime));
+        MyBuffManager.Add("AttackSpeedBuff", new AttackSpeedBuff(8f, MyBuffManager, WSkill().basic.name, WSkill().UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats), "Blood Hunt"));
+        myStats.wCD = WSkill().basic.coolDown[myStats.wLevel];
+        simulationManager.AddCastLog(myCastLog, 1);
     }
 
     public override IEnumerator ExecuteE()
     {
+        if (myStats.eLevel == -1) yield break;
         if (!CheckForAbilityControl(checksE)) yield break;
 
         if (!eCast)
@@ -87,12 +100,14 @@ public class Warwick : ChampionCombat
             yield return new WaitForSeconds(0.5f);
             TargetBuffManager.Add("FearBuff", new FleeBuff(1f, TargetBuffManager, ESkill().basic.name));
             eCast = false;
-            myStats.eCD = myStats.eSkill[0].basic.coolDown[myStats.eLevel] - timeSinceE;
+            myStats.eCD = ESkill().basic.coolDown[myStats.eLevel] - timeSinceE;
         }
+        simulationManager.AddCastLog(myCastLog, 2);
     }
 
     public override IEnumerator ExecuteR()
     {
+        if (myStats.rLevel == -1) yield break;
         if (!CheckForAbilityControl(checksR)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(RSkill().basic.castTime));
@@ -101,9 +116,10 @@ public class Warwick : ChampionCombat
         MyBuffManager.Add("Channeling", new ChannelingBuff(1.5f, MyBuffManager, RSkill().basic.name, "InfiniteDuress"));
         TargetBuffManager.Add("SuppressBuff", new SuppressionBuff(1.5f, TargetBuffManager, RSkill().basic.name));
 
-        UpdateTotalDamage(ref rSum, 3, new Damage(RSkill().UseSkill(myStats.rLevel, rKeys[0], myStats, targetStats), SkillDamageType.Spell, SkillComponentTypes.Blockable | SkillComponentTypes.Dash | SkillComponentTypes.OnHit), QSkill().basic.name);
+        UpdateTotalDamage(ref rSum, 3, new Damage(RSkill().UseSkill(myStats.rLevel, rKeys[0], myStats, targetStats), SkillDamageType.Spell,skillComponentType:(SkillComponentTypes)35736), RSkill().basic.name);
         UpdateTotalHeal(ref rSum, RSkill().UseSkill(myStats.rLevel, rKeys[0], myStats, targetStats), RSkill().basic.name); //need to change to post mitigation
         myStats.rCD = RSkill().basic.coolDown[myStats.rLevel];
+        simulationManager.AddCastLog(myCastLog, 4);
     }
 
     public override IEnumerator ExecuteA()
@@ -111,12 +127,16 @@ public class Warwick : ChampionCombat
         if (!CheckForAbilityControl(checksA)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(0.1f));
-        AutoAttack(new Damage(myStats.AD, SkillDamageType.Phyiscal));
-        if (myStats.currentHealth < 0.5f * myStats.maxHealth)
-        {
-            if (myStats.currentHealth > 0.25f * myStats.maxHealth) UpdateTotalHeal(ref pSum, 10 + 2 * myStats.level, myStats.passiveSkill.skillName); //need to fix post mitigation damage
-            else UpdateTotalHeal(ref pSum, 2.5f * (10 + 2 * myStats.level), myStats.passiveSkill.skillName);
+        if (UpdateTotalDamage(ref aSum, 5, new Damage(myStats.AD, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)5912), "Warwick's Auto Attack") != float.MinValue)
+		{
+            UpdateTotalDamage(ref aSum, 5, new Damage(10 + 2 * myStats.level, SkillDamageType.Spell, skillComponentType: (SkillComponentTypes)808), myStats.passiveSkill.name);
+            if (myStats.currentHealth < 0.5f * myStats.maxHealth)
+            {
+                if (myStats.currentHealth > 0.25f * myStats.maxHealth) UpdateTotalHeal(ref pSum, 10 + 2 * myStats.level, myStats.passiveSkill.skillName); //need to fix post mitigation damage
+                else UpdateTotalHeal(ref pSum, 2.5f * (10 + 2 * myStats.level), myStats.passiveSkill.skillName);
+            }
         }
-        AutoAttack(new Damage(10 + 2 * myStats.level, SkillDamageType.Spell, SkillComponentTypes.ProcDamage | SkillComponentTypes.OnHit));
+        attackCooldown = 1f / myStats.attackSpeed;
+        simulationManager.AddCastLog(myCastLog, 5);
     }
 }
