3f7aea3a771e8428fc6d0f020672799eb2018b2d
recep-sen <devrecepsen@gmail.com>
2022-12-03 13:49:13 +0300

    Yone updated

--- Assets/Scripts/Combat/Champions/Yone.cs
+++ Assets/Scripts/Combat/Champions/Yone.cs
@@ -2,11 +2,12 @@ using Simulator.Combat;
 using System.Collections;
 using UnityEngine;
 
-public class Yone : ChampionCombat                  //add passive ap damage when damage class added 
+public class Yone : ChampionCombat
 {
     public int qStack = 0;
     public float EDamage = 0;
     public bool InE = false;
+    public bool IsAzakanaSword = false;
     public static float GetYoneQCastTime(float bonusAS)
     {
         return bonusAS switch
@@ -95,28 +96,12 @@ public class Yone : ChampionCombat                  //add passive ap damage when
         checksE.Add(new CheckIfImmobilize(this));
         checksR.Add(new CheckIfImmobilize(this));
         targetCombat.checkTakeDamagePostMitigation.Add(new CheckForYoneE(targetCombat, this));
-        targetCombat.checkTakeDamagePostMitigation.Add(new CheckForYoneE(targetCombat, this));
         checksQ.Add(new CheckIfUnableToAct(this));
         checksW.Add(new CheckIfUnableToAct(this));
         checksE.Add(new CheckIfUnableToAct(this));
         checksR.Add(new CheckIfUnableToAct(this));
         checksA.Add(new CheckIfUnableToAct(this));
         checkTakeDamagePostMitigation.Add(new CheckShield(this));
-        checkTakeDamagePostMitigation.Add(new CheckShield(this));
-
-        myStats.qSkill[0].basic.castTime = GetYoneQCastTime(myStats.bonusAS);
-        myStats.qSkill[0].basic.coolDown[0] = GetYoneQCooldown(myStats.bonusAS);
-        myStats.qSkill[0].basic.coolDown[1] = GetYoneQCooldown(myStats.bonusAS);
-        myStats.qSkill[0].basic.coolDown[2] = GetYoneQCooldown(myStats.bonusAS);
-        myStats.qSkill[0].basic.coolDown[3] = GetYoneQCooldown(myStats.bonusAS);
-        myStats.qSkill[0].basic.coolDown[4] = GetYoneQCooldown(myStats.bonusAS);
-        myStats.wSkill[0].basic.castTime = GetYoneWCastTime(myStats.bonusAS);
-        myStats.wSkill[0].basic.coolDown[0] = GetYoneWCooldown(myStats.bonusAS);
-        myStats.wSkill[0].basic.coolDown[1] = GetYoneWCooldown(myStats.bonusAS);
-        myStats.wSkill[0].basic.coolDown[2] = GetYoneWCooldown(myStats.bonusAS);
-        myStats.wSkill[0].basic.coolDown[3] = GetYoneWCooldown(myStats.bonusAS);
-        myStats.wSkill[0].basic.coolDown[4] = GetYoneWCooldown(myStats.bonusAS);
-
 
         qKeys.Add("Physical Damage");
         wKeys.Add("Physical Damage");
@@ -130,53 +115,89 @@ public class Yone : ChampionCombat                  //add passive ap damage when
 
     public override IEnumerator ExecuteQ()
     {
+        if (myStats.qLevel == -1) yield break;
         if (!CheckForAbilityControl(checksQ)) yield break;
         if (qStack == 2 && myStats.buffManager.HasImmobilize) yield break;
-        yield return StartCoroutine(StartCastingAbility(myStats.qSkill[0].basic.castTime));
-        if (qStack != 2) StartCoroutine(QStack());
+        yield return StartCoroutine(StartCastingAbility(GetYoneQCastTime(myStats.bonusAS)));  
+        if (qStack != 2)
+        {
+            UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[0],skillComponentTypes: (SkillComponentTypes)14128);
+            StartCoroutine(QStack());
+        }
         else
         {
-            targetStats.buffManager.buffs.Add("Airborne", new AirborneBuff(0.75f, targetStats.buffManager, myStats.qSkill[0].basic.name));
+            if(UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], myStats.qLevel, qKeys[0], skillComponentTypes: (SkillComponentTypes)14130) != float.MinValue)
+                TargetBuffManager.Add("Airborne", new AirborneBuff(0.75f, targetStats.buffManager, myStats.qSkill[0].basic.name));
             qStack = 0;
             StopCoroutine(QStack());
             StopCoroutine(QStack());
         }
-        UpdateTotalDamage(ref qSum, 0, myStats.qSkill[0], 4, qKeys[0]);
-        myStats.qCD = myStats.qSkill[0].basic.coolDown[4];
+        myStats.qCD = GetYoneQCooldown(myStats.bonusAS);
+        simulationManager.AddCastLog(myCastLog, 0);
     }
 
     public override IEnumerator ExecuteW()
     {
+        if (myStats.wLevel == -1) yield break;
         if (!CheckForAbilityControl(checksW)) yield break;
 
-        yield return StartCoroutine(StartCastingAbility(myStats.wSkill[0].basic.castTime));
-        UpdateTotalDamage(ref wSum, 1, new Damage(myStats.wSkill[0].UseSkill(4, wKeys[0], myStats, targetStats), SkillDamageType.Phyiscal), myStats.wSkill[0].basic.name);
-        UpdateTotalDamage(ref wSum, 1, new Damage(myStats.wSkill[0].UseSkill(4, wKeys[1], myStats, targetStats), SkillDamageType.Spell), myStats.wSkill[0].basic.name);
+        yield return StartCoroutine(StartCastingAbility(GetYoneWCastTime(myStats.bonusAS)));
+        if(UpdateTotalDamage(ref wSum, 1, new Damage(myStats.wSkill[0].UseSkill(myStats.wLevel, wKeys[0], myStats, targetStats), SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)18560), myStats.wSkill[0].basic.name) != float.MinValue)
+            UpdateTotalDamage(ref wSum, 1, new Damage(myStats.wSkill[0].UseSkill(myStats.wLevel, wKeys[1], myStats, targetStats), SkillDamageType.Spell, skillComponentType: (SkillComponentTypes)16384), myStats.wSkill[0].basic.name);
         myStats.buffManager.shields.Add(myStats.wSkill[0].basic.name, new ShieldBuff(1.5f, myStats.buffManager, myStats.wSkill[0].basic.name, 2 * (35 + (20 / 17 * (myStats.level - 1)) + (myStats.bonusAD * 0.55f)), myStats.wSkill[0].basic.name));
-        myStats.wCD = myStats.wSkill[0].basic.coolDown[4];
+        myStats.wCD = GetYoneWCooldown(myStats.bonusAS);
+        simulationManager.AddCastLog(myCastLog, 1);
     }
 
     public override IEnumerator ExecuteE()
     {
+        if (myStats.eLevel == -1) yield break;
         if (!CheckForAbilityControl(checksE)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(myStats.eSkill[0].basic.castTime));
+        UpdateTotalDamage(ref eSum, 2, new Damage(0, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)2050), ESkill().basic.name);
         EDamage = 0;
         StartCoroutine(SoulUnbound());
-        myStats.eCD = myStats.eSkill[0].basic.coolDown[4];
+        myStats.eCD = myStats.eSkill[0].basic.coolDown[myStats.eLevel];
+        simulationManager.AddCastLog(myCastLog, 2);
     }
 
     public override IEnumerator ExecuteR()
     {
+        if (myStats.rLevel == -1) yield break;
         if (!CheckForAbilityControl(checksR)) yield break;
 
         yield return StartCoroutine(StartCastingAbility(myStats.rSkill[0].basic.castTime));
-        targetStats.buffManager.buffs.Add("Airborne", new AirborneBuff(1.05f, targetStats.buffManager, myStats.rSkill[0].basic.name));
-        myStats.buffManager.buffs.Add("UnableToAct", new UnableToActBuff(0.3f, myStats.buffManager, myStats.rSkill[0].basic.name));
-        myStats.rCD = myStats.rSkill[0].basic.coolDown[2];
-        yield return new WaitForSeconds(0.3f);
-        UpdateTotalDamage(ref rSum, 3, new Damage(myStats.rSkill[0].UseSkill(2, rKeys[0], myStats, targetStats), SkillDamageType.Phyiscal), myStats.rSkill[0].basic.name);
-        UpdateTotalDamage(ref rSum, 3, new Damage(myStats.rSkill[0].UseSkill(2, rKeys[1], myStats, targetStats), SkillDamageType.Spell), myStats.rSkill[0].basic.name);
+        if(UpdateTotalDamage(ref rSum, 3, new Damage(0, SkillDamageType.Phyiscal, (SkillComponentTypes)2177), RSkill().basic.name) != float.MinValue)
+        {
+            TargetBuffManager.Add("Airborne", new AirborneBuff(1.05f, targetStats.buffManager, myStats.rSkill[0].basic.name));
+            MyBuffManager.Add("UnableToAct", new UnableToActBuff(0.3f, myStats.buffManager, myStats.rSkill[0].basic.name));
+            yield return new WaitForSeconds(0.3f);
+            UpdateTotalDamage(ref rSum, 3, new Damage(myStats.rSkill[0].UseSkill(myStats.rLevel, rKeys[0], myStats, targetStats), SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)16384), myStats.rSkill[0].basic.name);
+            UpdateTotalDamage(ref rSum, 3, new Damage(myStats.rSkill[0].UseSkill(myStats.rLevel, rKeys[1], myStats, targetStats), SkillDamageType.Spell, skillComponentType: (SkillComponentTypes)16384), myStats.rSkill[0].basic.name);
+        }
+        myStats.rCD = myStats.rSkill[0].basic.coolDown[myStats.rLevel];
+        simulationManager.AddCastLog(myCastLog, 3);
+    }
+
+    public override IEnumerator ExecuteA()
+    {
+        if (!CheckForAbilityControl(checksA)) yield break;
+
+        yield return StartCoroutine(StartCastingAbility(0.1f));
+        if (IsAzakanaSword)
+        {
+            if (UpdateTotalDamage(ref aSum, 5, new Damage(myStats.AD * 0.5f, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)5912), "Yone's Auto Attack") != float.MinValue)
+                UpdateTotalDamage(ref aSum, 5, new Damage(myStats.AD * 0.5f, SkillDamageType.Spell, skillComponentType: (SkillComponentTypes)32), "Yone's Auto Attack");
+            IsAzakanaSword= false;
+        }
+        else
+        {
+            UpdateTotalDamage(ref aSum, 5, new Damage(myStats.AD, SkillDamageType.Phyiscal, skillComponentType: (SkillComponentTypes)5912), "Yone's Auto Attack");
+            IsAzakanaSword = true;
+        }
+        attackCooldown = 1f / myStats.attackSpeed;
+        simulationManager.AddCastLog(myCastLog, 5);
     }
 
     public override IEnumerator HijackedR(int skillLevel)
@@ -203,7 +224,7 @@ public class Yone : ChampionCombat                  //add passive ap damage when
         InE = true;
         yield return new WaitForSeconds(5f);
         InE = false;
-        UpdateTotalDamage(ref eSum, 2, new Damage(EDamage, SkillDamageType.True), "Soul Unbound");
+        UpdateTotalDamage(ref eSum, 2, new Damage(EDamage, SkillDamageType.True, skillComponentType: (SkillComponentTypes)18434), "Soul Unbound");
         StartCoroutine(StartCastingAbility(0.25f));
     }
 }
